d310a7070fe203486aaac81a77d9546ee36ba22f
syedarhamali <arham426@gmail.com>
2020-09-04 00:50:37 +0500

    add timer

--- app.js
+++ app.js
@@ -292,3 +292,29 @@ function result_show(){
     push_text.innerHTML = text_value;
     
 }
+
+// timer function 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+            result();
+            result_show();
+        }
+    }, 1000);
+}
+
+function timer() {
+    var tenSeconds = 60 * 1;
+        display = document.querySelector('#time');
+    startTimer(tenSeconds, display);
+}
